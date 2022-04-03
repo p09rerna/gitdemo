3a1c3782cc34ee7a97972a64751e82d8160384a1
p09rerna <prernaraj001@gmail.com>
2022-04-03 16:49:51 +0530

    get started with DOM

---
+++ dom.js
@@ -0,0 +1,16 @@
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.getElementById('header-title'));
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+console.log('headerTitle');
+headerTitle.style.borderBottom = 'solid 3px #000';
+header.style.font='bold';
+header.style.textColor='green';
