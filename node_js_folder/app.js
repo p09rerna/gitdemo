12200b81191f93c3e818c7b68e18f4eaf3a36d03
p09rerna <prernaraj001@gmail.com>
2022-05-04 18:50:43 +0530

    home,about and node pages done.

--- node_js_folder/app.js
+++ node_js_folder/app.js
@@ -1,7 +1,33 @@
-const http = require('http');
+const http= require('http');
 const server = http.createServer((req,res)=>{
-     console.log(req);
-     console.log('Prerna Raj');
-}); 
+const url=req.url;
+ if(url==='/home'){
+     res.write('<html>');
+     res.write('<head><title>Home Page</title></head>');
+     res.write('<body><h1>Welcome home</h1></body>');
+     res.write('</html>');
+     return res.end();
+ }
+if(url==='/about'){
+     res.write('<html>');
+     res.write('<head><title>About Page</title></head>');
+     res.write('<body><h1>Welcome to About us page</h1></body>');
+     res.write('</html>');
+     return res.end();
+}
+if(url==='/node'){
+     res.write('<html>');
+     res.write('<head><title>Node Project Page</title></head>');
+     res.write('<body><h1>Welcome to my Node.js Project</h1></body>');
+     res.write('</html>');
+     return res.end();
+}
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title></head>');
+res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+res.write('</html>');
+res.end();
+});  
 
 server.listen(4000);
