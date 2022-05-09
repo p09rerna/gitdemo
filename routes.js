36c7c2dc4bf498b03444602121648943047450bd
p09rerna <prernaraj001@gmail.com>
2022-05-10 00:17:29 +0530

    clean up done

---
+++ routes.js
@@ -0,0 +1,52 @@
+const fs= require('fs');
+const requestHandler=(req,res)=>{
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+       const content = fs.readFileSync('message.txt','utf8');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>');
+        res.write(content);
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==="/message" && method=== "POST"){
+           const body = [];
+           req.on('data',(chunk) =>{
+               console.log(chunk);
+                body.push(chunk);
+           });
+          return req.on('end', () => {
+               const parsedBody= Buffer.concat(body).toString();
+               const message= parsedBody.split('=')[1];
+               fs.writeFile('message.txt',message, err =>{
+               if(err){
+               return console.log("Error:Something Went Wrong");
+               }
+               res.statusCode=302;
+               res.setHeader('Location','/');
+               return res.end();
+            });
+           });
+    }
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title></head>');
+res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+res.write('</html>');
+res.end();
+};
+
+//module.exports = requestHandler;
+/*module.exports = {
+    handler: requestHandler,
+    someText: 'Some hard coded text'
+};*/
+//module.exports.handler=requestHandler;
+//module.exports.someText='Some hard coded text';
+
+exports.handler=requestHandler;
+exports.someText='Some hard coded text';
