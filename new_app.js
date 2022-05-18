b49821bd9125b9b72141b21aaf63e399bd3be2a4
p09rerna <prernaraj001@gmail.com>
2022-05-19 00:54:29 +0530

    started with express.

--- new_app.js
+++ new_app.js
@@ -1,5 +1,10 @@
-const http = require('http');
-const routes= require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const express = require('express');
+const app= express();
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+});
+app.listen(9100);
