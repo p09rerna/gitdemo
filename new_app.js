6d57cb62fe7b638ea2c0aaed29dba398ca98dc97
p09rerna <prernaraj001@gmail.com>
2022-05-19 15:00:05 +0530

    routing and filtering done.

--- new_app.js
+++ new_app.js
@@ -1,17 +1,16 @@
 const express = require('express');
+const adminRoutes= require('./routes/admin.js');
+const shopRoutes= require('./routes/shop.js');
 const bodyParser= require('body-parser');
 const app= express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</buttton></form>');
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>');
 });
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express!</h1>');
-});
+
 app.listen(9100);
