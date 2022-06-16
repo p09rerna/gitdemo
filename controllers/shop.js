75c52047a7fe3cac70beca9c40125f121995c482
p09rerna <prernaraj001@gmail.com>
2022-06-16 22:44:00 +0530

    update delete and get product done

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,8 +2,8 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll()
+  .then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
@@ -15,8 +15,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+ /* Product.findAll({where: {id: prodId} })
+  .then(products => {
+    res.render('shop/product-detail', {
+      product: product[0],
+      pageTitle: products[0].title,
+      path: '/products'
+    });
+  })
+  .catch(err => console.log(err));*/
   Product.findById(prodId)
-  .then(([product]) => {
+  .then(product => {
     res.render('shop/product-detail', {
       product: product[0],
       pageTitle: product.title,
@@ -27,8 +36,8 @@ exports.getProduct = (req, res, next) => {
   };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-.then(([rows, fieldData]) => {
+  Product.findAll()
+.then(products => {
   res.render('shop/index', {
     prods: products,
     pageTitle: 'Shop',
